0383e1fcfd6eb60977ec0f54ac070d0b0373231e
James Michael Kuizon <jameskuizon13@gmail.com>
2022-06-11 23:20:14 +0800

    Added a working signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+
 import { DatabaseService } from '../database/database.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
   constructor(private databaseService: DatabaseService) {}
 
-  signup() {
-    return 'I am sign up 1';
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.databaseService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Duplicate account');
+        }
+      } else {
+        throw error;
+      }
+    }
   }
 
   signin() {
